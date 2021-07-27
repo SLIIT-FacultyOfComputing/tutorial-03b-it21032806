9f78df3497b865b5b66eb73519d277c8c2b5c52c
it21032806 <87432677+it21032806@users.noreply.github.com>
2021-07-27 10:16:15 +0000

    completed

--- exercise02.cpp
+++ exercise02.cpp
@@ -28,6 +28,12 @@ int main() {
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
+    }
+int volume(int height, int width, int length){
+int answer;
+
+answer = height * width * length;
+return answer;
 }
 
 // Implement the Volume() function here
